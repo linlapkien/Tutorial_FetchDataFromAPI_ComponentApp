421f0f4d5802e690c821ab92ec7264c6b6e97383
Lin Lập Kiến <linlapkien@gmail.com>
2024-08-20 11:46:50 +0700

    A component app for Host app to remote and pass props on it

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,29 @@
+import NextFederationPlugin from '@module-federation/nextjs-mf';
+
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
+  webpack(config, options) {
+    const { isServer } = options;
+    config.plugins.push(
+      new NextFederationPlugin({
+        name: 'component_app',
+        remotes: {},
+        filename: 'static/chunks/remoteEntry.js',
+        exposes: {
+          './CocktailCard': './pages/CocktailCard.tsx',
+        },
+        extraOptions: {
+          debug: false, // `false` by default
+          exposePages: false, // `false` by default
+        },
+        shared: {},
+      })
+    );
+
+    return config;
+  },
 };
 
 export default nextConfig;
